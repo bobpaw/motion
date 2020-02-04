13f2c91e745c8f4ded5969bbb00c42caa6a656d1
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-02-03 18:36:39 -0600

    Add a point whose velocity you can control

--- src/main.cc
+++ src/main.cc
@@ -25,6 +25,7 @@ int main (int argc, char* argv[]) {
 		return -1;
 	}
 	graphics_renderer = SDL_CreateRenderer(graphics_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Use hardware acceleration and vsync
+	int x = 320, y = 200, v_x = 1, v_y = 1, k_timeout_max = 20, k_timeout = 0;
 	while (window_quit == false) {
 		while (SDL_PollEvent(&event) != 0) { // SDL_PollEvent automatically updates key_state array
 			if (event.type == SDL_QUIT) {
@@ -33,10 +34,39 @@ int main (int argc, char* argv[]) {
 			}
 		}
 		// Handle keyboard input
+		if (k_timeout > 0) {
+			--k_timeout;
+		} else {
+			if (key_state[SDL_SCANCODE_UP] && !key_state[SDL_SCANCODE_DOWN]) {
+				--v_y;
+			} else if (!key_state[SDL_SCANCODE_UP] && key_state[SDL_SCANCODE_DOWN]) {
+				++v_y;
+			}
+			if (key_state[SDL_SCANCODE_RIGHT] && !key_state[SDL_SCANCODE_LEFT]) {
+				++v_x;
+			} else if (!key_state[SDL_SCANCODE_RIGHT] && key_state[SDL_SCANCODE_LEFT]) {
+				--v_x;
+			}
+			if (key_state[SDL_SCANCODE_0]) {
+				v_x = 1;
+				v_y = 1;
+			}
+			k_timeout = k_timeout_max;
+		}
+		if (x + v_x >= ScreenWidth || x + v_x < 0) v_x = -v_x;
+		if (y + v_y >= ScreenHeight || y + v_y < 0) v_y = -v_y;
+		x += v_x;
+		y += v_y;
+		SDL_SetRenderDrawColor(graphics_renderer, 0, 0, 0, 0);
 		SDL_RenderClear(graphics_renderer); // Cover screen in a black rectangle, effectively clearing the screen
 		// Render sprites to screen
+		SDL_SetRenderDrawColor(graphics_renderer, 255, 255, 255, 255);
+		SDL_RenderDrawPoint(graphics_renderer, x, y);
+		SDL_RenderDrawPoint(graphics_renderer, x + 1, y);
+		SDL_RenderDrawPoint(graphics_renderer, x, y + 1);
+		SDL_RenderDrawPoint(graphics_renderer, x + 1, y + 1);
 		SDL_RenderPresent(graphics_renderer); // Update screen based on changes
-		// Using VSYNC SDL_Delay(20); // Wait 20 milliseconds, should blip 50 fps
+		// SDL_Delay(20); // Wait 20 milliseconds, should blip 50 fps
 	}
 	SDL_RenderClear(graphics_renderer);
 	SDL_DestroyRenderer(graphics_renderer);
