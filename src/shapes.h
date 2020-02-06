0d3ae039fc5f640976ea77680508bfeadf31074d
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-02-05 20:26:22 -0600

    Draw circle with modified Bresenhams
    Added shapes.cc to test shape functions

--- src/shapes.h
+++ src/shapes.h
@@ -6,10 +6,31 @@
 
 #ifndef MOTION_SHAPES_H
 #define MOTION_SHAPES_H
-int DrawCircle(SDL_Renderer* renderer, int x, int y, double r) {
-	for (int i = 0; i < 360; i += 10) {
-		SDL_RenderDrawPoint(renderer, int(x + r * std::cos(i * M_PI / 180.0)), int(y + r * std::sin(i * M_PI / 180.0)));
-	}
-	return 1;
+static int DrawCircle_pts(SDL_Renderer* renderer, int cx, int cy, int x, int y) {
+	SDL_RenderDrawPoint(renderer, cx + x, cy + y);
+	SDL_RenderDrawPoint(renderer, cx + x, cy - y);
+	SDL_RenderDrawPoint(renderer, cx - x, cy + y);
+	SDL_RenderDrawPoint(renderer, cx - x, cy - y);
+	SDL_RenderDrawPoint(renderer, cx + y, cy + x);
+	SDL_RenderDrawPoint(renderer, cx + y, cy - x);
+	SDL_RenderDrawPoint(renderer, cx - y, cy + x);
+	SDL_RenderDrawPoint(renderer, cx - y, cy - x);
+	return 0;
+}
+
+int DrawCircle(SDL_Renderer* renderer, int cx, int cy, double r) {
+  int x = r, y = 0, dx = 1 - 2 * r, dy  = 1, RE = 0;
+  while (x >= y) {
+		DrawCircle_pts(renderer, cx, cy, x, y);
+    ++y;
+		RE += dy;
+		dy += 2;
+		if (2 * RE + dx > 0) {
+			--x;
+			RE += dx;
+			dx += 2;
+		}
+  }
+  return 0;
 }
 #endif // MOTION_SHAPES_H
