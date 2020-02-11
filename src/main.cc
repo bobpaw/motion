22b045ada6faef0fbc31ded74c5283562594d9d9
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-02-11 17:21:04 -0600

    Blit ball out instead of just circle
    Default direction is 0deg

--- src/main.cc
+++ src/main.cc
@@ -13,8 +13,7 @@
 const int ScreenWidth = 640;
 const int ScreenHeight = 400;
 
-
-#undef main
+	#undef main
 int main (int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		std::cerr << "SDL couldn't initialize! SDL_ERROR: " << SDL_GetError() << std::endl;
@@ -33,10 +32,18 @@ int main (int argc, char* argv[]) {
 		return -1;
 	}
 	graphics_renderer = SDL_CreateRenderer(graphics_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Use hardware acceleration and vsync
+	SDL_Texture* ball = SDL_CreateTexture(graphics_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 21, 21);
 	int k_timeout_max = 1, k_timeout = 0;
 	phys::Plane screen(ScreenHeight, ScreenWidth);
-	screen.makeParticle(320, 200, phys::PVector(1, 45.0));
+	screen.makeParticle(320, 200, phys::PVector(1, 0.0));
 	screen.particles[0].radius = 10;
+	SDL_SetRenderDrawColor(graphics_renderer, 255, 255, 255, 255);
+	SDL_SetRenderTarget(graphics_renderer, ball);
+	for (int i = 1; i < 11; ++i) DrawCircle(graphics_renderer, 10, 10, i);
+	SDL_SetRenderTarget(graphics_renderer, NULL);
+	SDL_Rect dest;
+	dest.w = 21;
+	dest.h = 21;
 	while (window_quit == false) {
 		while (SDL_PollEvent(&event) != 0) { // SDL_PollEvent automatically updates key_state array
 			if (event.type == SDL_QUIT) {
@@ -60,7 +67,7 @@ int main (int argc, char* argv[]) {
 			}
 			if (key_state[SDL_SCANCODE_0]) {
 				screen.particles[0].velocity.mag = 1;
-				screen.particles[0].velocity.dir = 45;
+				screen.particles[0].velocity.dir = 0;
 			}
 			k_timeout = k_timeout_max;
 		}
@@ -72,9 +79,9 @@ int main (int argc, char* argv[]) {
 		SDL_SetRenderDrawColor(graphics_renderer, 0, 0, 0, 0);
 		SDL_RenderClear(graphics_renderer); // Cover screen in a black rectangle, effectively clearing the screen
 		// Render sprites to screen
-		SDL_SetRenderDrawColor(graphics_renderer, 255, 255, 255, 255);
-		SDL_RenderDrawPoint(graphics_renderer, int(screen.particles[0].x), int(screen.particles[0].y));
-		DrawCircle(graphics_renderer, screen.particles[0].x, screen.particles[0].y, screen.particles[0].radius);
+		dest.x = static_cast<int>(screen.particles[0].x - screen.particles[0].radius);
+		dest.y = static_cast<int>(screen.particles[0].y - screen.particles[0].radius);
+		SDL_RenderCopy(graphics_renderer, ball, NULL, &dest);
 		std::cout << "\rVelocity: " << screen.particles[0].velocity.mag << " Direction: " << screen.particles[0].velocity.dir;
 		SDL_RenderPresent(graphics_renderer); // Update screen based on changes
 		// SDL_Delay(20); // Wait 20 milliseconds, should blip 50 fps
