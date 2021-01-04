e21bee90c456563f7f295a2e7b6b64976f1155eb
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-01-03 20:10:30 -0600

    Reorganized main

--- src/main.cc
+++ src/main.cc
@@ -8,45 +8,51 @@
 
 #include "constants.h"
 
-const int ScreenWidth = 640;
-const int ScreenHeight = 400;
+constexpr int ScreenWidth = 640;
+constexpr int ScreenHeight = 400;
+constexpr int ball_radius = 5;
 
 #undef main
 int main (int argc, char* argv[]) {
 	std::mt19937 random_engine(std::random_device{}());
-	sf::RenderWindow graphics_window(sf::VideoMode(ScreenWidth, ScreenHeight), "Motion", sf::Style::Default /*, contenxt*/ ); // Window object
-	graphics_window.setVerticalSyncEnabled(true);
-	sf::Event event;
-	// const uint8_t* key_state = SDL_GetKeyboardState(nullptr); // Get address of keystate array and assign it to keyState pointer
-	constexpr int ball_radius = 5;
-	int k_timeout_max = 1, k_timeout_max_min = 1, k_timeout_max_max = 4, k_timeout = 0;
-	phys::Plane screen(ScreenHeight, ScreenWidth);
-	screen.makeParticle(320, 200, ball_radius, phys::PVector(.1f, 0.0));
-	sf::CircleShape ball(ball_radius);
-	ball.setFillColor(sf::Color::White);
-	// ball.transform something something turn on blending
-	std::uniform_real_distribution<float> widthR(ball_radius, ScreenWidth - ball_radius), heightR(ball_radius, ScreenHeight - ball_radius), degreeR(0, 360);
-	sf::Clock clock;
+	std::uniform_real_distribution<float> widthR(ball_radius, ScreenWidth - ball_radius),
+		heightR(ball_radius, ScreenHeight - ball_radius), degreeR(0, 360);
 
+	sf::RenderWindow graphics_window(sf::VideoMode(ScreenWidth, ScreenHeight), "Motion", sf::Style::Default, sf::ContextSettings(0, 0, 8));
+	graphics_window.setVerticalSyncEnabled(true);
+
+	// Our one ball shape
+	sf::CircleShape ball(ball_radius);
+
+	sf::Clock clock;
+	sf::Event event;
+	
+	// Texty
 	sf::Font freesans;
 	freesans.loadFromFile("./FreeSans.ttf");
 
-	sf::Text pCount("Particles: 1", freesans);
-	pCount.setCharacterSize(16);
+	sf::Text pCount("Particles: 1", freesans, 16);
 
+	// Awkward timers so holding keys down doesn't get handled too fast; probably do with an sf::Clock eventually
+	int k_timeout_max = 1, k_timeout_max_min = 1, k_timeout_max_max = 4, k_timeout = 0;
+	
+	phys::Plane screen(ScreenHeight, ScreenWidth);
+	screen.makeParticle(320, 200, ball_radius, phys::PVector(.1f, 0.0));
 
+	clock.restart();
 	while (graphics_window.isOpen()) {
-		while (graphics_window.pollEvent(event)) { // SDL_PollEvent automatically updates key_state array
+		while (graphics_window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
-				// Check if X button (in top right) has been pressed
 				graphics_window.close();
 			}
 		}
-		// Handle keyboard input
+
+		// Handle keys that can be held down
 		if (k_timeout > 0) {
 			--k_timeout;
 		} else {
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Equal) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Dash) && screen.particles.size() < 200) {
+				// The random engine doesn't work correctly if they're inline calls in makeParticle
 				float x = widthR(random_engine), y = heightR(random_engine), dir = degreeR(random_engine);
 				screen.makeParticle(x, y, ball_radius, phys::PVector(.25f, dir));
 				k_timeout_max -= (k_timeout_max > k_timeout_max_min ? 1 : 0);
@@ -73,9 +79,8 @@ int main (int argc, char* argv[]) {
 			ball.setPosition(screen.x(i) - screen.radius(i), screen.y(i) - screen.radius(i));
 			graphics_window.draw(ball);
 		}
-		graphics_window.display(); // Update screen based on changes
-		// SDL_Delay(20); // Wait 20 milliseconds, should blip 50 fps
-		sf::sleep(sf::milliseconds(10));
+		graphics_window.display();
+		sf::sleep(sf::milliseconds(10)); // A number high enough that CPU doesn't overtake graphics output
 	}
 	return 0;
 }
