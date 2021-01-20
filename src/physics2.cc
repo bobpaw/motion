b7c11818aa7f040622dda851ab1fda9733f7f46e
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-01-20 09:28:23 -0600

    Started particle engine rewrite as sfml object
    Also uses vertex array and only one call to draw so hopefully it'll be faster
    Added math_library convencience variable to cmake

---
+++ src/physics2.cc
@@ -0,0 +1,85 @@
+#include "physics2.h"
+
+#include <cmath>
+
+namespace {
+	template <typename T>
+	static sf::Vector2<T> center (const sf::Rect<T>& r) {
+		return sf::Vector2<T>(r.left + r.width / 2, r.top + r.height / 2);
+	}
+
+	template <typename T>
+	static sf::Rect<T> boundSquare (const sf::Vector2<T>& center, float r) {
+		return sf::Rect<T>(center.x - r, center.y - r, r * 2, r * 2);
+	}
+
+	static inline float dot(const sf::Vector2f& v1, const sf::Vector2f& v2) {
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	static float distance(const sf::Vector2f& v1) {
+		return std::hypot(v1.x, v1.y);
+	}
+}
+namespace phys {
+	sf::CircleShape ParticleSystem::circle_shape(30);
+
+	sf::Vector2f ParticleSystem::get_center(size_t index) {
+		return sf::Vector2f(
+			(vertices[index].position.x + vertices[index + 2].position.x) / 2,
+			(vertices[index].position.y + vertices[index + 2].position.y) / 2);
+	}
+
+	void ParticleSystem::move_particle(sf::Time elapsed, size_t index) {
+		sf::Vector2f displacement = particles[index].velocity
+			* static_cast<float>(elapsed.asMilliseconds());
+		for (size_t i = 0; i < 4; ++i) {
+			vertices[index + i].position += displacement;
+		}
+
+		const sf::Vector2f &first = vertices[index].position, & third = vertices[index + 2].position;
+		if (first.x < bounds.left || third.x >= bounds.left + bounds.width)
+			particles[index].velocity.x *= -1.0f;
+		if (first.y < bounds.top || third.y >= bounds.top + bounds.height)
+			particles[index].velocity.y *= -1.0f;
+		move_particle(elapsed, index);
+	}
+
+	void ParticleSystem::collide_particles(size_t i, size_t j) {
+		sf::Vector2f v1, v2;
+		sf::Vector2f pos1 = get_center(i), pos2 = get_center(j);
+		Particle& alpha = particles[i], & beta = particles[j];
+		v1 = alpha.velocity - dot(alpha.velocity - beta.velocity, pos1 - pos2)
+			/ dot(pos1 - pos2, pos1 - pos2) * (pos1 - pos2);
+		v2 = beta.velocity - dot(beta.velocity - alpha.velocity, pos2 - pos1)
+			/ dot(pos2 - pos1, pos2 - pos1) * (pos2 - pos1);
+		alpha.velocity = v1;
+		beta.velocity = v2;
+	}
+
+	bool ParticleSystem::fast_infringe(size_t i, size_t j) {
+#ifndef FAST_FAST
+		sf::FloatRect r1(vertices[i].position, vertices[i + 2].position - vertices[i].position),
+			r2(vertices[j].position, vertices[j + 2].position - vertices[j].position);
+		return r1.intersects(r2);
+#else
+		sf::Vector2f& p1 = vertices[i].position, & p2 = vertices[j].position;
+		return /* boolean expression */
+#endif
+	}
+
+	bool ParticleSystem::slow_infringe(size_t i, size_t j) {
+		// FIXME: It's possible the <= should be a <
+		return distance(vertices[j].position - vertices[i].position)
+			<= particles[i].radius + particles[j].radius;
+	}
+
+	void ParticleSystem::update(const sf::Time& elapsed) {
+		for (size_t i = 0; i < particles.size(); ++i)
+			move_particle(elapsed, i);
+		for (size_t f = 0; f < particles.size() - 1; ++f)
+			for (size_t o = f + 1; o < particles.size() - 1; ++o)
+				if (fast_infringe(f, o) && slow_infringe(f, o))
+					collide_particles(f, o);
+	}
+}
---
+++ src/physics2.h
@@ -0,0 +1,49 @@
+#include <vector>
+#include <cmath>
+#include <cassert>
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+#ifndef MOTION_PHYSICS2_H_
+#define MOTION_PHYSICS2_H_
+
+namespace phys {
+	class ParticleSystem: sf::Drawable, sf::Transformable {
+		struct Particle {
+			sf::Vector2f velocity;
+			float radius;
+		};
+		std::vector<Particle> particles;
+		sf::VertexArray vertices;
+		sf::FloatRect bounds;
+		ParticleSystem(float w, float h):
+			bounds(0.f, 0.f, w, h),
+			particles(),
+			vertices() {}
+		void move_particle(sf::Time elapsed, size_t index);
+		void collide_particles(size_t i, size_t j);
+		void update(const sf::Time& elapsed);
+		size_t makeParticle(float x = 0.f, float y = 0.f, float r = 0.f, sf::Vector2f v = sf::Vector2f(0, 0));
+		size_t makeParticle(sf::Vector2f pos = {0, 0}, float r = 0.f, sf::Vector2f v = {0, 0});
+	private:
+
+		sf::Vector2f get_center(size_t index);
+		static sf::CircleShape circle_shape;
+		bool fast_infringe(size_t i, size_t j);
+		bool slow_infringe(size_t i, size_t j);
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+			// apply the entity's transform -- combine it with the one that was passed by the caller
+			states.transform *= getTransform(); // getTransform() is defined by sf::Transformable
+
+			// apply the texture
+			states.texture = circle_shape.getTexture();
+
+			// you may also override states.shader or states.blendMode if you want
+
+			// draw the vertex array
+			target.draw(vertices, states);
+		}
+	};
+}
+#endif // MOTION_PHYSICS2_H_
